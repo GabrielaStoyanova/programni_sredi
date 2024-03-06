042425c6cfd0f6f1507749b74232fbebcc4a313e
Gabriela Stoyanova <92469013+GabrielaStoyanova@users.noreply.github.com>
2024-03-06 14:41:18 +0200

    Add files via upload

---
+++ Upraznenie_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Welcome.Model;
+using Welcome.Others;
+using Welcome.View;
+using Welcome.ViewModel;
+
+namespace Welcome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            User user = new User();
+            user.Names = "Gaby Stoyanova";
+            user.Password = "password123";
+            user.Role = UserRolesEnum.ADMIN;
+            user.facultyNumber = "121221000";
+            user.email = "example@tu-sofia.bg";
+
+            UserViewModel userViewModel = new UserViewModel(user);
+            UserView userView = new UserView(userViewModel);
+            userView.Display();
+        }
+    }
+}
