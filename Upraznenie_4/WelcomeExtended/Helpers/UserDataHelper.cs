07b67d0e94215394a7d9a904dcd871f9644f4a8f
Gabriela Stoyanova <92469013+GabrielaStoyanova@users.noreply.github.com>
2024-03-21 00:33:06 +0200

    Add files via upload

---
+++ Upraznenie_4/WelcomeExtended/Helpers/UserDataHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+using WelcomeExtended.Data;
+
+namespace WelcomeExtended.Helpers
+{
+    public static class UserDataHelper
+    {
+        public static bool ValidateCredentials(this UserData userData, string name, string password)
+        {
+            // Check if name or password is empty
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(password))
+                return false;
+
+            // Call the ValidateUserLinq method of the UserData collection
+            return userData.ValidateUserLinq(name, password);
+        }
+
+        public static User GetUser(this UserData userData, string name, string password)
+        {
+            // Call the GetUser method of the UserData collection
+            return userData.GetUser(name, password);
+        }
+    }
+}
---
+++ Upraznenie_4/WelcomeExtended/Others/Delegates.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using WelcomeExtended.Helpers;
+
+namespace WelcomeExtended.Others
+{
+    public class Delegates
+    {
+        public static readonly ILogger logger = LoggerHelper.GetLogger("Hello");
+        private static readonly ILogger _errorLogger = LoggerHelper.GetErrorLogger("ErrorLogger");
+        public static void Log(string error)
+        {
+            logger.LogError(error);
+        }
+        public static void Log2(string error) 
+        {
+            Console.WriteLine("- DELEGATES -");
+            Console.WriteLine($"{error}");
+            Console.WriteLine("- DELEGATES -");
+        }
+        public static void Log3(string error)
+        {
+            // Log some messages
+            logger.LogInformation("Information message => Task 4");
+            logger.LogWarning("Warning message => Task 4");
+            logger.LogError("Error message => Task 4");
+        }
+
+        public static void Log4(string logMessage)
+        {
+            // Log the provided message
+            logger.LogInformation(logMessage);
+        }
+
+        public static void Log5(string logMessage)
+        {
+            // Log the provided message
+            _errorLogger.LogInformation(logMessage);
+        }
+    }
+}
