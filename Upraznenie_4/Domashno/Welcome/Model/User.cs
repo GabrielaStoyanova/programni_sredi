09a100c8516795098c7e875bc6a4f5c34b2f9abb
Gabriela Stoyanova <92469013+GabrielaStoyanova@users.noreply.github.com>
2024-03-21 23:46:27 +0200

    Add files via upload

---
+++ Upraznenie_4/Domashno/Welcome/Model/User.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Others;
+
+namespace Welcome.Model
+{
+    public class User
+    {
+        private string _names = "";
+        private string _password = "";
+        private UserRolesEnum _role;
+        private string _facultyNumber = "";
+        private string _email = "";
+        private int shift = 3; // Shift for Caesar cipher
+
+        // New private variables from the 3 exercise
+        private int _id;
+        private DateTime _expires;
+
+        public string Names {
+            get { return _names; }
+            set {  _names = value; }
+        }
+        public string Password {
+            get { return Decrypt(_password); }
+            set { _password = Encrypt(value); } 
+        }
+
+        private string Encrypt(string input){ 
+            // Simple Caesar cipher encryption
+            char[] charArray = input.ToCharArray(); 
+            for (int i = 0; i < charArray.Length; i++){ 
+                charArray[i] = (char)(charArray[i] + shift); 
+            } 
+            return new string(charArray); 
+        }
+
+        // Wrapper method for encryption, because of the database comparison 
+        // from 4 exercise
+        public string EncryptPassword(string password)
+        {
+            return Encrypt(password);
+        }
+
+        private string Decrypt(string input){ 
+            // Simple Caesar cipher decryption
+            char[] charArray = input.ToCharArray(); 
+            for (int i = 0; i < charArray.Length; i++) { 
+                charArray[i] = (char)(charArray[i] - shift); 
+            } 
+            return new string(charArray); 
+        }
+
+        public UserRolesEnum Role {
+            get { return _role; }
+            set {_role = value;}
+        }
+        public string facultyNumber
+        {
+            get { return _facultyNumber; }
+            set { _facultyNumber = value; }
+        }
+        public string email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        //New public properties from the 3 exercise
+        public virtual int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public DateTime Expires
+        {
+            get { return _expires; }
+            set { _expires = value; }
+        }
+        //public User(string names, string password, UserRolesEnum role)
+        //{
+        //Names = names;
+        //Password = password;
+        //Role = role;
+        //}
+        //public User(string names, string password, UserRolesEnum role)
+        //{
+        //this.Names = names;
+        //this.Password = password;
+        //this.Role = role;
+        //}
+        public override string ToString()
+        {
+            return $"Name: {Names}, Role: {Role}, Faculty Number: {facultyNumber}, Email: {email}";
+        }
+    }
+}
---
+++ Upraznenie_4/Domashno/WelcomeExtended/Loggers/LoggerProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WelcomeExtended.Loggers
+{
+    public class LoggerProvider : ILoggerProvider
+    {
+        private readonly string _filePath;
+
+        public LoggerProvider(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            
+
+            // Create a FileLogger instance for writing to a file
+            ILogger fileLogger = new FileLogger(_filePath);
+
+            // Create a HashLogger instance for other logging purposes
+            ILogger hashLogger = new HashLogger(categoryName);
+
+            // Create a FileLoggerSuccessLogin instance for other logging purposes
+            ILogger fileLoggerSuccessLogin = new FileLoggerSuccessLogin(_filePath);
+
+            // Return the FileLogger instance
+            // return fileLogger;
+            // Return the HashLogger instance
+            // return hashLogger;
+            // Return the FileLoggerSuccessLogin instance
+            return fileLoggerSuccessLogin;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
