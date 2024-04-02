611655512e1f34d6af034e30e3f298d45d2de361
Gabriela Stoyanova <92469013+GabrielaStoyanova@users.noreply.github.com>
2024-04-02 19:00:41 +0300

    Add files via upload

---
+++ Upraznenie 5/PS_43_Gaby_Stoyanova/WelcomeExtended/Helpers/UserHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+
+namespace WelcomeExtended.Helpers
+{
+    public static class UserHelper
+    {
+        public static string ToString(this User user)
+        {
+            return $"Name: {user.Names}, Role: {user.Role}, Faculty Number: {user.facultyNumber}, Email: {user.email}";
+            // I'm getting Welcome.Model.User printed to the console instead of the user details,
+            // which maybe comes from the ToString method of the User class is not overridden to
+            // provide the desired string representation of the user object, thats why i made this 
+            // method also in the User class => WORKED
+        }
+    }
+}
